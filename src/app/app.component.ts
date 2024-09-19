b6a168b0b0814712c5ecc8adbf6ef9a9e2a0a40d
NghiNV <nghinv03@gmail.com>
2024-09-19 11:13:00 +0700

    Complete layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {HeaderComponent} from "./header/header.component";
+import {UserInputComponent} from "./user-input/user-input.component";
+import {InvestmentResultsComponent} from "./investment-results/investment-results.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, UserInputComponent, InvestmentResultsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
